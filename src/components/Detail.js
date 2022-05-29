2b448aab56dfa78fa9767d31872df504d3f27332
yuyanyang97 <yuyanyang97@gmail.com>
2022-05-30 02:23:03 +0800

    insert cart and calculate teh price

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,25 +1,23 @@
 import Cart from "./Cart"
 
 const Detail = ({listCart,deleteData}) => {
+    const total_ini = 0;
+      const subtotal = listCart.map(amount => amount.price)
+      .reduce( (total, amount) => total + amount, total_ini );
     return(
         <div>
             <p>Detail</p>
             {
                 listCart.map((cart) => {
-                    
-                    const {id,name, price} = cart;
-                    
-                    return (
-                        <Cart 
-                        id={id}
-                        name={name}
-                        price={price}
-                        deleteData={deleteData} />
-                    );
+                    const {name, price, id} = cart;
+                    return  <Cart name={name} price={price} id={id} deleteData={deleteData} />
                 })
             }
+            
+            {subtotal}
+            
         </div>
-    );
+    )
 }
 
 export default Detail
